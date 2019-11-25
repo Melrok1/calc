c11928fca5e8339dbf2f3d119d9823503c507bc4
Melrok <martin.blascak86@gmail.com>
2019-11-25 22:08:32 +0100

    html a css 80%

--- calc/src/App.vue
+++ calc/src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
-   
+    <div id="calcWrap">
+      <input type="text">
+      <div id="buttonWrap">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>C</button>
+        <button>0</button>
+        <button>.</button>
+        <button>/</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +35,50 @@ export default {
 </script>
 
 <style>
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
 
+  #app {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  #calcWrap {
+    display: flex;
+    justify-content: stretch;
+    align-items: center;
+    flex-direction: column;
+    width: 40%;
+    height: 40%;
+    background: rgba(9, 45, 143, 0.767);
+    box-shadow: 3px 3px 2px #858585;
+  }
+
+  #calcWrap input[type=text]{
+    display: block;
+    margin: 1rem auto;
+    height: 3rem;
+    width: 85%;
+    padding: 0.25rem 0.5rem;
+    font-size: 2rem;
+  }
+
+  #buttonWrap {
+    margin: 0 auto;
+    width: 85%;
+    min-height: 200px;
+    border: 2px solid #a2a2a2;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+  }
+
+  #buttonWrap button {
+    font-size: 2rem;
+  }
 </style>
